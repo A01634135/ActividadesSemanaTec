81af47214c2442ae1637d8899c45dfbee34260bf
A01634135 <A01634135@itesm.mx>
2021-09-17 01:59:42 -0500

    Implementar la convolucion de una imagen con un filtro Prewitt vertical

--- Act1/convolution.py
+++ Act1/convolution.py
@@ -1,21 +1,31 @@
+"""
+Programa que recibe una imagen y una matriz, e implementa su convolución
+"""
+
 import numpy as np
 import cv2
 import argparse
 import matplotlib.pyplot as plt
  
-#Multiplica dos matrices y devuelve su suma
+
+""" Multiplica dos matrices y devuelve su suma """
 def conv_helper(fragment, kernel):
     
     f_row, f_col = fragment.shape
     k_row, k_col = kernel.shape 
     result = 0.0
+
     for row in range(f_row):
         for col in range(f_col):
             result += fragment[row,col] *  kernel[row,col]
+
     return result
 
-#Realiza convolucion y devuelve la matriz resultante
+
+""" Realiza la convolucion y devuelve la matriz resultante """
 def convolution(image, kernel):
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) #vuelve la imagen a blanco y negro
 
     image_row, image_col = image.shape #asigna alto y ancho de la imagen 
     kernel_row, kernel_col = kernel.shape #asigna alto y ancho del filtro
@@ -29,15 +39,16 @@ def convolution(image, kernel):
                                     col:col + kernel_col],kernel)
              
     plt.imshow(output, cmap='gray')
-    plt.title("Output Image using {}X{} Kernel".format(kernel_row, kernel_col))
+    plt.title("Imagen output usando {}X{} Kernel".format(kernel_row, kernel_col))
     plt.show()
  
     return output
 
-if __name__ == '__main__':
 
-    filter = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]) #matriz del filtro
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--image", required=True, help="Path to the image")
-    args = vars(ap.parse_args())
-    image = cv2.imread(args["image"])
+filtro = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]]) #Prewitt (My)
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+image = cv2.imread(args["image"])
+
+convolution(image, filtro)
